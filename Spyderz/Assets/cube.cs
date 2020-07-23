7acdfb05ef8edb24b8a61cd4c1852801fed6f875
Miguel Cabrera <miguel.cabrera101@gmail.com>
2020-07-22 21:45:14 -0400

    added square and movement

---
+++ Spyderz/Assets/cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Input.GetAxis("Horizontal"));
+        float hMover = Input.GetAxis("Horizontal");
+        float vMover = Input.GetAxis("Vertical");
+        transform.Translate(0f * Time.deltaTime, 1f * Time.deltaTime*hMover, 1f*Time.deltaTime*vMover);
+    }
+}
